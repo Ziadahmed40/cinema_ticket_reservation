6837e590929230c0d010a3bbd3da674444aaa985
zeyad ahmed <zeyadmokled@hotmail.com>
2025-05-23 16:12:50 +0300

    modify movies in databse(add rating)

--- reservations/models.py
+++ reservations/models.py
@@ -7,7 +7,7 @@ class Movie(models.Model):
     duration = models.IntegerField()  # in minutes
     genre = models.CharField(max_length=50)
     image_url = models.CharField(max_length=255, blank=True, null=True)
-
+    rating=models.FloatField()
     def __str__(self):
         return self.title
 
